52240542bb86f0a9f4a235e09976b5336e299ce4
azzelwee <azzelswee@gmail.com>
2025-02-09 08:16:41 +0800

    Initial commit

---
+++ src/Portfolio.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+      <h2>Explore Base Designs</h2>
+      <p>This is where you can showcase all your Once Human base designs.</p>
+    </div>
+  );
+}
+
+export default Portfolio;
